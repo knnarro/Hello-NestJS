860548966a3d18d14f21350dcaf87d3f95a703ef
knnarro <knnarro@gmail.com>
2023-09-20 01:17:37 +0900

    fix: 비밀번호에 암호화 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { genSalt, hash } from 'bcryptjs'
 import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -13,7 +14,10 @@ export class AuthService {
 
     async createUser(authCredentialDto: AuthCredentialDto): Promise<User>{
         const {username, password} = authCredentialDto;
-        const user = this.userRepository.create({username, password});
+
+        const salt = await genSalt();
+        const hashedPassword = await hash(password, salt);
+        const user = this.userRepository.create({username, password: hashedPassword});
         try{
             return await this.userRepository.save(user);
         } catch (error){
