5ab7a796ca0fa9527f2f524dd6878ebed258bafa
knnarro <knnarro@gmail.com>
2023-09-19 13:20:49 +0900

    feat: board repository 생성

---
+++ src/boards/boards.repository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Board } from "./boards.entity";
+
+@EntityRepository(Board)
+export class BoardRepository extends Repository<Board> {
+    
+}
